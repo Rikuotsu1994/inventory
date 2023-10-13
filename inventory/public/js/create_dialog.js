69071072f8e2314848df5e0faa289a8cfaec408a
Rikuotsu1994 <luck5151riku@gmail.com>
2023-10-13 20:22:33 +0900

    fixed seasoning create function for jsfile

--- inventory/public/js/create_dialog.js
+++ inventory/public/js/create_dialog.js
@@ -1,31 +1,62 @@
 document.querySelector(".create_seasoning_image").addEventListener("click", () => {
   document.querySelector("input.create_seasoning_image").click();
   });
-
-function imagePreview(event) {
-  var file = event.target.files[0];
-  var reader = new FileReader();
-  var preview = document.getElementById("img_preview");
-  var previewImage = document.getElementById("previewImage");
-
-  if(previewImage != null) {
-    preview.removeChild(previewImage);
+document.getElementById("create_seasoning_image").addEventListener("change", function(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  const createimage = document.getElementById("create_img");
+  const createimgpreview = document.getElementById("create_img_preview");
+  if(createimage != null) {
+    createimgpreview.removeChild(createimage);
   }
-
   reader.onload = function(event) {
-    var img = document.createElement("img");
+    const img = document.createElement("img");
     img.setAttribute("src", reader.result);
-    img.setAttribute("id", "previewImage");
-    img.setAttribute("class", "previewImage");
-    preview.appendChild(img);
+    img.setAttribute("id", "create_img");
+    img.setAttribute("class", "create_img");
+    createimgpreview.appendChild(img);
   };
   reader.readAsDataURL(file);
-};
-
+});
 document.getElementById("create_btn").addEventListener("click", () => {
   document.querySelector(".create_dialog").style.display = "block";
 });
 document.querySelector(".create_cancel_btn").addEventListener("click", () => {
+  const errormessage = document.querySelectorAll(".error_message");
+  const imagefile = document.getElementById("create_seasoning_image");
+  const createdialog = document.querySelector(".create_dialog_error");
+  const createimage = document.getElementById("create_img");
+  const createimgpreview = document.getElementById("create_img_preview");
+  const createnameerror = document.querySelector(".create_seasoning_name_error");
+  const createinventoryerror = document.querySelector(".create_seasoning_inventory_error");
+  const createremarkserror = document.querySelector(".create_seasoning_remarks_error");
+
+  if(imagefile !== null) {
+    imagefile.value = "";
+  }
+  if (createimage != null) {
+    createimgpreview.removeChild(createimage);
+  }
+
+  if (createdialog != null) {
+    createdialog.classList.remove("create_dialog_error");
+    if (errormessage != null)
+      errormessage.forEach(x => {
+        x.remove("error_message"); 
+    });
+    if (createnameerror != null) {
+      document.querySelector(".create_seasoning_name_label_error").classList.replace("create_seasoning_name_label_error", "create_seasoning_name_label");
+      createnameerror.classList.replace("create_seasoning_name_error", "create_seasoning_name");
+    }
+    if (createinventoryerror != null) {
+      document.querySelector(".create_seasoning_inventory_label_error").classList.replace("create_seasoning_inventory_label_error", "create_seasoning_inventory_label");
+      createinventoryerror.classList.replace("create_seasoning_inventory_error", "create_seasoning_inventory");
+    }
+    if (createremarkserror != null) {
+      document.querySelector(".create_remarks_label_error").classList.replace("create_remarks_label_error", "create_remarks_label");
+      createremarkserror.classList.replace("create_seasoning_remarks_error", "create_seasoning_remarks");
+    }
+  }
   document.querySelector(".create_dialog").style.display = "none";
 });
 function digitcontrol(inputdigit, maxdigit) {  
--- inventory/resources/views/components/create.blade.php
+++ inventory/resources/views/components/create.blade.php
@@ -1,5 +1,5 @@
 <x-dialog>
-  @if (($errors->has('dialog_name')) == "create_dialog" )
+  @if ($errors->has('dialog_name'))
     <x-slot name="dialog_name">create_dialog create_dialog_error</x-slot>
   @else
     <x-slot name="dialog_name">create_dialog</x-slot>
@@ -11,34 +11,34 @@
       <div class="create_seasoning_form">
         <div class="create_seasoning_name_area">
           <label for="create_seasoning_name">
-          @if ((($errors->has('dialog_name')) == "create_dialog" ) && ($errors->has('seasoning_name')))
-            <div class="seasoning_name_label_error">調味料名</div></label>
+          @if (($errors->has('dialog_name')) && ($errors->has('seasoning_name')))
+            <div class="create_seasoning_name_label_error">調味料名</div></label>
             <input type="text" name="seasoning_name" class="create_seasoning_name_error" id="create_seasoning_name" value="{{ old('seasoning_name') }}" maxlength="50" required>
             <div class="error_message">{{ $errors->first('seasoning_name') }}</div>
           @else
-            <div class="seasoning_name_label">調味料名</div></label>
+            <div class="create_seasoning_name_label">調味料名</div></label>
             <input type="text" name="seasoning_name" class="create_seasoning_name" id="create_seasoning_name" value="{{ old('seasoning_name') }}" maxlength="50" required>
           @endif
         </div>
         <div class="create_seasoning_inventory_area">
           <label for="create_seasoning_inventory">
-          @if ((($errors->has('dialog_name')) == "create_dialog" ) && ($errors->has('seasoning_inventory')))
-            <div class="seasoning_inventory_label_error">在庫数</div></label>
+          @if (($errors->has('dialog_name')) && ($errors->has('seasoning_inventory')))
+            <div class="create_seasoning_inventory_label_error">在庫数</div></label>
             <input type="number" name="seasoning_inventory" class="create_seasoning_inventory_error" id="create_seasoning_inventory" value="{{ old('seasoning_inventory') }}" min="0" max="99" oninput="digitcontrol(this, 2)">
             <div class="error_message">{{ $errors->first('seasoning_inventory') }}</div>
           @else
-            <div class="seasoning_inventory_label">在庫数</div></label>
+            <div class="create_seasoning_inventory_label">在庫数</div></label>
             <input type="number" name="seasoning_inventory" class="create_seasoning_inventory" id="create_seasoning_inventory" value="{{ old('seasoning_inventory') }}" min="0" max="99" oninput="digitcontrol(this, 2)">
           @endif
         </div>
         <div class="create_seasoning_remarks_area">
           <label for="create_seasoning_remarks">
-          @if ((($errors->has('dialog_name')) == "create_dialog" ) && ($errors->has('remarks')))
-            <div class="remarks_label_error">メモ</div></label>
+          @if (($errors->has('dialog_name')) && ($errors->has('remarks')))
+            <div class="create_remarks_label_error">メモ</div></label>
             <textarea name="remarks" maxlength="100" class="create_seasoning_remarks_error" id="create_seasoning_remarks" value="{{ old('remarks') }}"></textarea >
             <div class="error_message">{{ $errors->first('remarks') }}</div>
           @else
-            <div class="remarks_label">メモ</div></label>
+            <div class="create_remarks_label">メモ</div></label>
             <textarea name="remarks" maxlength="100" class="create_seasoning_remarks" id="create_seasoning_remarks" value="{{ old('remarks') }}"></textarea >
           @endif
         </div>
@@ -49,15 +49,15 @@
             </svg>
             <div class="image_annotation">画像は1MBまで</div>
             <div class="image_extension">(対応形式jpeg・jpg・png)</div>
-            <input type="file" name="seasoning_image" class="create_seasoning_image" id="create_seasoning_image" accept=".jpg,.jpeg,.png" onchange="imagePreview(event)">
+            <input type="file" name="seasoning_image" class="create_seasoning_image" id="create_seasoning_image" accept=".jpg,.jpeg,.png">
           </label>
-          @if ((($errors->has('dialog_name')) == "create_dialog" ) && ($errors->has('seasoning_image')))
+          @if (($errors->has('dialog_name')) && ($errors->has('seasoning_image')))
             <div class="error_message">{{ $errors->first('seasoning_image') }}</div>
           @endif
         </div>
         <div class="preview_annotation">画像プレビュー</div>
         <div class="image_area">
-          <div class="img_preview" id="img_preview"></div>
+          <div class="create_img_preview" id="create_img_preview"></div>
         </div>
         <div class="form_btn">
           <input type="reset" value="キャンセル" class="create_cancel_btn">
